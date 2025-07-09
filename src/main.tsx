4086805ca8ae5b4383c535c2d98db35cd0c3bdec
Alex Kombo <alexkombo@gmail.com>
2025-07-09 17:40:35 +0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,10 @@ import App from './App.tsx';
 import './index.css';
 import Hotjar from '@hotjar/browser';
 
-const supabaseUrl = import.meta.env.VITE_HO;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const siteId = import.meta.env.VITE_HOTJAR_SITE_ID;
+const hotjarVersion = import.meta.env.VITE_HOTJAR_VERSION;
+
+Hotjar.init(parseInt(siteId), parseInt(hotjarVersion));
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
